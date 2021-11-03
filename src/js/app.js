3ed3dd25a8339a6e286ab26047af66f0569cd2ee
Antoni <antoni.protoni@gmail.com>
2021-11-02 21:08:49 -0300

    change variables

--- src/js/app.js
+++ src/js/app.js
@@ -31,15 +31,35 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  const {
+    name,
+    lastname,
+    role,
+    city,
+    country,
+    twitter,
+    github,
+    linkedin,
+    instagram,
+    socialMediaPosition
+  } = variables;
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${name ? name : "Your Name"}
+              ${lastname ? lastname : ""}
+          </h1>
+          
+          <h2>${role ? role : "web developer"}</h2>
+
+          <h3> ${city ? city : "Miami"}
+               ${country ? country : "USA"}
+          </h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              twitter ? twitter : "https://twitter.com/rocketseat"
+            } ><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
